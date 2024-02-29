0bb2bcf87c47a981da93f72373f1b7972316ec00
N00budrag0n <helmiagung2468@gmail.com>
2024-02-29 15:14:19 +0700

    Update migrations

--- database/migrations/2024_02_29_074309_create_t_penjualan_detail_table.php
+++ database/migrations/2024_02_29_074309_create_t_penjualan_detail_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->integer('harga');
             $table->integer('jumlah');
             $table->timestamps();
+            //
 
             // FK penjualan_id
             $table->foreign('penjualan_id')->references('penjualan_id')->on('t_penjualan');
