1c48ff573ba78d03cf3cd0536a96b3f9fa5d2843
N00budrag0n <helmiagung2468@gmail.com>
2024-03-07 14:10:15 +0700

    Pertemuan4_Praktikum2.3_RetrievingAggregates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,23 +10,8 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $data =
-        // [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12356'),
-        // ];
-        // UserModel::create($data);
-        // UserModel::where('username','customer-1')->update($data);
-        
-        // $user = UserModel::firstWhere('level_id', 1);
-        // $user = UserModel::findOr(1, ['username', 'nama'], function(){
-        //     abort(404);
-        // });
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        });
+        $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
         return view('user', ['data' => $user]);
     }
 }
