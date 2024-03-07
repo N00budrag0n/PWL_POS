b6f39affbc1be49c4b56f37dd5c0399c82a9aa18
N00budrag0n <helmiagung2468@gmail.com>
2024-03-07 14:21:11 +0700

    Pertemuan4_Prakitkum2.2_NotFoundExceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('level_id', 2)->count();
-        // dd($user);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
