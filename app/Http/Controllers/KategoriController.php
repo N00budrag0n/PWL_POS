3776eb584104cf6200004e6696c909bf750089a3
N00budrag0n <helmiagung2468@gmail.com>
2024-03-15 22:48:30 +0700

    Pertemuan5_Praktikum3_Form

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\KategoriModel;
 use App\DataTables\KategoriDataTable;
 
 class KategoriController extends Controller
@@ -11,5 +12,19 @@ class KategoriController extends Controller
     public function index(KategoriDataTable $dataTable)
     {
         return $dataTable->render('kategori.index');
+    }
+
+    public function create()
+    {
+        return view('kategori.create');
+    }
+
+    public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+        return redirect('/kategori');
     }
 }
