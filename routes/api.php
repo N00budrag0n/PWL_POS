5753d5c84e32442a0add6475126c128f678e0479
N00budrag0n <helmiagung2468@gmail.com>
2024-05-16 06:53:47 +0700

    Pertemuan10_Praktikum4_CRUD

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\Api\LevelController;
+
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request){
